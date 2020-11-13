d8357567e6bc9ce7c71a3bdd8eb7fae2d568cca4
yuuta.oowaki <oy11222@i.softbank.jp>
2020-11-13 02:27:40 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  get 'users/create'
+  root to: "toppages#index"
+  
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
 end
